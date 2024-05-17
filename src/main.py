75bf7f2eef2ae1925ccfdb628147b69bf3f3a88f
Delta-Chat <adele.desmazi@gmail.com>
2024-05-17 21:14:32 +0200

    Init files

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import pygame as pg
+import pygame_gui as pg_gui
+
+
+def main():
+	print("Hello World")
+
+
+if __name__=="__main__":
+	main()
---
+++ src/terrain.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class Item(Enum):
+    Grenade = 0
+    Bazooka = 1
+    Teleporter = 2
+    PneumaticDrill = 3
+
